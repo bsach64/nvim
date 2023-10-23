923e930960f8969d20f7d26c6d716262e063a269
Bhavik Sachdev <b.sachdev1904@gmail.com>
2023-10-24 00:34:10 +0530

    removing things

--- lua/whichkey.lua
+++ lua/whichkey.lua
@@ -109,9 +109,6 @@ local mappings = {
 
     t = {
         name = "Terminal",
-        n = { "<cmd>lua _NODE_TOGGLE()<cr>", "Node" }, -- Node Terminal
-        t = { "<cmd>lua _HTOP_TOGGLE()<cr>", "Htop" }, -- (Optional) Htop, If you have htop in linux
-        p = { "<cmd>lua _PYTHON_TOGGLE()<cr>", "Python" }, -- Python Terminal
         f = { "<cmd>ToggleTerm direction=float<cr>", "Float" }, -- Floating Terminal
 
         -- Play with size according to your needs.
