a908890f5ba8ba687419d1e773f611342c77ae1d
Bhavik Sachdev <b.sachdev1904@gmail.com>
2024-05-17 12:11:45 +0530

    configuring

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -378,40 +378,40 @@ PLUGINS = {
 	},
 
 	{ -- Autoformat
-		"stevearc/conform.nvim",
-		lazy = false,
-		keys = {
-			{
-				"<leader>f",
-				function()
-					require("conform").format({ async = true, lsp_fallback = true })
-				end,
-				mode = "",
-				desc = "[F]ormat buffer",
-			},
-		},
-		opts = {
-			notify_on_error = false,
-			format_on_save = function(bufnr)
-				-- Disable "format_on_save lsp_fallback" for languages that don't
-				-- have a well standardized coding style. You can add additional
-				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
-				return {
-					timeout_ms = 500,
-					lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
-				}
-			end,
-			formatters_by_ft = {
-				lua = { "stylua" },
-				-- Conform can also run multiple formatters sequentially
-				-- python = { "isort", "black" },
-				--
-				-- You can use a sub-list to tell conform to run *until* a formatter
-				-- is found.
-				-- javascript = { { "prettierd", "prettier" } },
-			},
-		},
+		-- "stevearc/conform.nvim",
+		-- lazy = false,
+		-- keys = {
+		-- 	{
+		-- 		"<leader>f",
+		-- 		function()
+		-- 			require("conform").format({ async = true, lsp_fallback = true })
+		-- 		end,
+		-- 		mode = "",
+		-- 		desc = "[F]ormat buffer",
+		-- 	},
+		-- },
+		-- opts = {
+		-- 	notify_on_error = false,
+		-- 	format_on_save = function(bufnr)
+		-- 		-- Disable "format_on_save lsp_fallback" for languages that don't
+		-- 		-- have a well standardized coding style. You can add additional
+		-- 		-- languages here or re-enable it for the disabled ones.
+		-- 		local disable_filetypes = { c = true, cpp = true }
+		-- 		return {
+		-- 			timeout_ms = 500,
+		-- 			lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+		-- 		}
+		-- 	end,
+		-- 	formatters_by_ft = {
+		-- 		lua = { "stylua" },
+		-- 		-- Conform can also run multiple formatters sequentially
+		-- 		-- python = { "isort", "black" },
+		-- 		--
+		-- 		-- You can use a sub-list to tell conform to run *until* a formatter
+		-- 		-- is found.
+		-- 		-- javascript = { { "prettierd", "prettier" } },
+		-- 	},
+		-- },
 	},
 
 	{ -- Autocompletion
@@ -537,6 +537,10 @@ PLUGINS = {
 			vim.api.nvim_set_hl(0, "NvimTreeEmptyFolderName", { bold = false })
 			vim.api.nvim_set_hl(0, "NvimTreeOpenedFolderName", { bold = false })
 		end,
+		-- "Biscuit-Theme/nvim",
+		-- lazy = false,
+		-- priority = 1000,
+		-- config = function()
 		-- "Shatur/neovim-ayu",
 	},
 
@@ -644,5 +648,8 @@ PLUGINS = {
 		"akinsho/toggleterm.nvim",
 		version = "*",
 		config = true,
+	},
+	{
+		"ThePrimeagen/vim-be-good",
 	},
 }
