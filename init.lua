acb02316990957847358bb1e6c632483b759c2ca
Bhavik Sachdev <b.sachdev1904@gmail.com>
2026-05-03 03:25:19 +0530

    hidden files and update theme

--- init.lua
+++ init.lua
@@ -13,6 +13,8 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- plugins --
+-- old theme
+-- "https://github.com/WTFox/jellybeans.nvim",
 vim.pack.add({
 	"https://github.com/tpope/vim-sleuth",
 	"https://github.com/lewis6991/gitsigns.nvim",
@@ -21,7 +23,7 @@ vim.pack.add({
 	"https://github.com/nvim-telescope/telescope.nvim",
 	"https://github.com/nvim-lualine/lualine.nvim",
 	"https://github.com/stevearc/oil.nvim",
-	"https://github.com/WTFox/jellybeans.nvim",
+	"https://github.com/oskarnurm/koda.nvim",
 	"https://github.com/neovim/nvim-lspconfig",
 	"https://github.com/tpope/vim-fugitive",
 	"https://github.com/rafamadriz/friendly-snippets",
@@ -42,8 +44,14 @@ require("gitsigns").setup({
 require("mini.pairs").setup({})
 require("telescope").setup({})
 require("lualine").setup({ options = { theme = "auto" } })
-require("oil").setup({})
-require("jellybeans").setup({ italics = false })
+require("oil").setup({
+	keymaps = {
+		["g."] = "actions.toggle_hidden",
+	},
+	view_options = {
+		show_hidden = false,
+	},
+})
 -- grn (to rename current variable)
 -- grr (get all references in quick fix list)
 -- crtl + S in insert mode tells you signature of func
@@ -58,7 +66,7 @@ vim.lsp.enable({"gopls", "rust_analyzer", "clangd", "pyright", "bash-language-se
 local languages = { 'rust', 'c', 'python', 'go', 'lua', 'bash' }
 require("nvim-treesitter").install(languages)
 
-vim.cmd("colorscheme jellybeans-muted")
+vim.cmd("colorscheme koda-moss")
 
 -- keymaps --
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>") -- clear search with Esc
